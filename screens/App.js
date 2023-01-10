30b5c2f0786a112fc975374e81de09ce24247e9c
munnaSorder <developermunnaa@gmail.com>
2023-01-10 23:00:42 +0600

    initial add

---
+++ screens/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View className="bg-blue-300 flex-1 justify-center items-center">
+      <Text className="text-red-600">Hello Worldss</Text>
+    </View>
+  );
+};
+
+export default App;
